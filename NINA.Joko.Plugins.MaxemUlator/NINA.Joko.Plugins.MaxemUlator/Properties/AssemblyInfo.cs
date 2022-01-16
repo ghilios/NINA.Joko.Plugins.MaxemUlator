6c074858f124cfeb9b31cd4f277deb89fb26f58e
George Hilios <ghilios@gmail.com>
2022-01-16 10:49:06 -0500

    Rename project

---
+++ NINA.Joko.Plugins.MaxemUlator/NINA.Joko.Plugins.MaxemUlator/Properties/AssemblyInfo.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// [MANDATORY] The following GUID is used as a unique identifier of the plugin. Generate a fresh one for your plugin!
+[assembly: Guid("141704e0-505c-492c-9e74-3b85f1c05d12")]
+
+// [MANDATORY] The assembly versioning
+//Should be incremented for each new release build of a plugin
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+
+// [MANDATORY] The name of your plugin
+[assembly: AssemblyTitle("MyPlugin")]
+// [MANDATORY] A short description of your plugin
+[assembly: AssemblyDescription("This is the description of my plugin. It should contain all relevant description of what the plugin is about.")]
+
+
+// The following attributes are not required for the plugin per se, but are required by the official manifest meta data
+
+// Your name
+[assembly: AssemblyCompany("MyCompanyOrName")]
+// The product name that this plugin is part of
+[assembly: AssemblyProduct("MyPluginProduct")]
+[assembly: AssemblyCopyright("")]
+
+// The minimum Version of N.I.N.A. that this plugin is compatible with
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "2.0.0.2020")]
+
+// The license your plugin code is using
+[assembly: AssemblyMetadata("License", "MPL-2.0")]
+// The url to the license
+[assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
+// The repository where your pluggin is hosted
+[assembly: AssemblyMetadata("Repository", "https://bitbucket.org/Isbeorn/nina.plugin.template/")]
+
+
+// The following attributes are optional for the official manifest meta data
+
+//[Optional] Your plugin homepage URL - omit if not applicaple
+[assembly: AssemblyMetadata("Homepage", "")]
+
+//[Optional] Common tags that quickly describe your plugin
+[assembly: AssemblyMetadata("Tags", "Template,Sequencer")]
+
+//[Optional] A link that will show a log of all changes in between your plugin's versions
+[assembly: AssemblyMetadata("ChangelogURL", "https://bitbucket.org/Isbeorn/nina.plugin.template/commits/branch/master")]
+
+//[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
+[assembly: AssemblyMetadata("FeaturedImageURL", "")]
+//[Optional] A url to an example screenshot of your plugin in action
+[assembly: AssemblyMetadata("ScreenshotURL", "")]
+//[Optional] An additional url to an example example screenshot of your plugin in action
+[assembly: AssemblyMetadata("AltScreenshotURL", "")]
+//[Optional] An in-depth description of your plugin
+[assembly: AssemblyMetadata("LongDescription", @"An in-depth description of your plugin")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// [Unused]
+[assembly: AssemblyConfiguration("")]
+// [Unused]
+[assembly: AssemblyTrademark("")]
+// [Unused]
+[assembly: AssemblyCulture("")]
